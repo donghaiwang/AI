bb2ededad748919ce75bbe6b4d298cd09b371c7f
donghaiwang <1402499358@qq.com>
2018-08-18 21:24:24 +0800

    add DQN readme

--- DQN-tensorflow/main.py
+++ DQN-tensorflow/main.py
@@ -8,21 +8,22 @@ from config import get_config
 
 flags = tf.app.flags
 
-# Model
-flags.DEFINE_string('model', 'm1', 'Type of model')
-flags.DEFINE_boolean('dueling', False, 'Whether to use dueling deep q-network')
-flags.DEFINE_boolean('double_q', False, 'Whether to use double q-learning')
+# 添加命令行的可选参数，可以在命令行使用-h查看帮助信息
+# 模型
+flags.DEFINE_string('model', 'm1', 'Type of model')  # 模型类型
+flags.DEFINE_boolean('dueling', False, 'Whether to use dueling deep q-network')  # 决定是否使用竞争深度Q网络（默认不使用）
+flags.DEFINE_boolean('double_q', False, 'Whether to use double q-learning')      # 决定是否使用双Q网络（默认不使用）
 
-# Environment
-flags.DEFINE_string('env_name', 'Breakout-v0', 'The name of gym environment to use')
-flags.DEFINE_integer('action_repeat', 4, 'The number of action to be repeated')
+# 仿真环境
+flags.DEFINE_string('env_name', 'Breakout-v0', 'The name of gym environment to use')  # 使用的gym仿真环境（默认是Breakout-v0）
+flags.DEFINE_integer('action_repeat', 4, 'The number of action to be repeated')       # 重复执行动作的次数（默认4次）
 
 # Etc
-flags.DEFINE_boolean('use_gpu', True, 'Whether to use gpu or not')
-flags.DEFINE_string('gpu_fraction', '1/1', 'idx / # of gpu fraction e.g. 1/3, 2/3, 3/3')
-flags.DEFINE_boolean('display', False, 'Whether to do display the game screen or not')
-flags.DEFINE_boolean('is_train', True, 'Whether to do training or testing')
-flags.DEFINE_integer('random_seed', 123, 'Value of random seed')
+flags.DEFINE_boolean('use_gpu', True, 'Whether to use gpu or not')                        # 是否使用GPU（默认使用GPU）
+flags.DEFINE_string('gpu_fraction', '1/1', 'idx / # of gpu fraction e.g. 1/3, 2/3, 3/3')  # GPU使用的比例（默认全部使用）
+flags.DEFINE_boolean('display', False, 'Whether to do display the game screen or not')    # 是否显示游戏画面（默认不显示）
+flags.DEFINE_boolean('is_train', True, 'Whether to do training or testing')               # 是否进行训练和测试（默认进行训练和测试）
+flags.DEFINE_integer('random_seed', 123, 'Value of random seed')                          # 使用的随机种子（默认123）
 
 FLAGS = flags.FLAGS
 
