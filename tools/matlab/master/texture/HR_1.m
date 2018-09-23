07a9f3ee9ba8c980623e4951f57859654bbf75d3
donghaiwang <1402499358@qq.com>
2018-09-23 12:05:27 +0800

    add texture

---
+++ tools/matlab/master/texture/HR_1.m
@@ -0,0 +1,253 @@
+clear
+load HR.090715.0000_moc.txt
+load HR.090715.0001_moc.txt
+load HR.090715.0002_moc.txt
+load HR.090715.0003_moc.txt
+load HR.090715.0004_moc.txt
+load HR.090715.0005_moc.txt
+load HR.090715.0006_moc.txt
+load HR.090715.0007_moc.txt
+load HR.090715.0008_moc.txt
+load HR.090715.0009_moc.txt
+
+load HR.090715.0010_moc.txt
+load HR.090715.0011_moc.txt
+load HR.090715.0012_moc.txt
+load HR.090715.0013_moc.txt
+load HR.090715.0014_moc.txt
+load HR.090715.0015_moc.txt
+load HR.090715.0016_moc.txt
+load HR.090715.0017_moc.txt
+load HR.090715.0018_moc.txt
+load HR.090715.0019_moc.txt
+
+load HR.090715.0020_moc.txt
+load HR.090715.0021_moc.txt
+load HR.090715.0022_moc.txt
+load HR.090715.0023_moc.txt
+load HR.090715.0024_moc.txt
+load HR.090715.0025_moc.txt
+load HR.090715.0026_moc.txt
+load HR.090715.0027_moc.txt
+load HR.090715.0028_moc.txt
+load HR.090715.0029_moc.txt
+
+load HR.090715.0030_moc.txt
+load HR.090715.0031_moc.txt
+load HR.090715.0032_moc.txt
+load HR.090715.0033_moc.txt
+load HR.090715.0034_moc.txt
+load HR.090715.0035_moc.txt
+load HR.090715.0036_moc.txt
+load HR.090715.0037_moc.txt
+load HR.090715.0038_moc.txt
+load HR.090715.0039_moc.txt
+
+load HR.090715.0040_moc.txt
+load HR.090715.0041_moc.txt
+load HR.090715.0042_moc.txt
+load HR.090715.0043_moc.txt
+load HR.090715.0044_moc.txt
+load HR.090715.0045_moc.txt
+load HR.090715.0046_moc.txt
+load HR.090715.0047_moc.txt
+load HR.090715.0048_moc.txt
+load HR.090715.0049_moc.txt
+
+load HR.090715.0050_moc.txt
+load HR.090715.0051_moc.txt
+load HR.090715.0052_moc.txt
+load HR.090715.0053_moc.txt
+load HR.090715.0054_moc.txt
+load HR.090715.0055_moc.txt
+load HR.090715.0056_moc.txt
+load HR.090715.0057_moc.txt
+load HR.090715.0058_moc.txt
+load HR.090715.0059_moc.txt
+
+load HR.090715.0060_moc.txt
+load HR.090715.0061_moc.txt
+load HR.090715.0062_moc.txt
+load HR.090715.0063_moc.txt
+load HR.090715.0064_moc.txt
+load HR.090715.0065_moc.txt
+
+x = HR_090715_0000_moc(:, 1);
+y0 = HR_090715_0000_moc(:, 2);
+y1 = HR_090715_0001_moc(:, 2);
+y2 = HR_090715_0002_moc(:, 2);
+y3 = HR_090715_0003_moc(:, 2);
+y4 = HR_090715_0004_moc(:, 2);
+y5 = HR_090715_0005_moc(:, 2);
+y6 = HR_090715_0006_moc(:, 2);
+y7 = HR_090715_0007_moc(:, 2);
+y8 = HR_090715_0008_moc(:, 2);
+y9 = HR_090715_0009_moc(:, 2);
+
+y10 = HR_090715_0010_moc(:, 2);
+y11 = HR_090715_0011_moc(:, 2);
+y12 = HR_090715_0012_moc(:, 2);
+y13 = HR_090715_0013_moc(:, 2);
+y14 = HR_090715_0014_moc(:, 2);
+y15 = HR_090715_0015_moc(:, 2);
+y16 = HR_090715_0016_moc(:, 2);
+y17 = HR_090715_0017_moc(:, 2);
+y18 = HR_090715_0018_moc(:, 2);
+y19 = HR_090715_0019_moc(:, 2);
+
+y20 = HR_090715_0020_moc(:, 2);
+y21 = HR_090715_0021_moc(:, 2);
+y22 = HR_090715_0022_moc(:, 2);
+y23 = HR_090715_0023_moc(:, 2);
+y24 = HR_090715_0024_moc(:, 2);
+y25 = HR_090715_0025_moc(:, 2);
+y26 = HR_090715_0026_moc(:, 2);
+y27 = HR_090715_0027_moc(:, 2);
+y28 = HR_090715_0028_moc(:, 2);
+y29 = HR_090715_0029_moc(:, 2);
+
+y30 = HR_090715_0030_moc(:, 2);
+y31 = HR_090715_0031_moc(:, 2);
+y32 = HR_090715_0032_moc(:, 2);
+y33 = HR_090715_0033_moc(:, 2);
+y34 = HR_090715_0034_moc(:, 2);
+y35 = HR_090715_0035_moc(:, 2);
+y36 = HR_090715_0036_moc(:, 2);
+y37 = HR_090715_0037_moc(:, 2);
+y38 = HR_090715_0038_moc(:, 2);
+y39 = HR_090715_0039_moc(:, 2);
+
+y40 = HR_090715_0040_moc(:, 2);
+y41 = HR_090715_0041_moc(:, 2);
+y42 = HR_090715_0042_moc(:, 2);
+y43 = HR_090715_0043_moc(:, 2);
+y44 = HR_090715_0044_moc(:, 2);
+y45 = HR_090715_0045_moc(:, 2);
+y46 = HR_090715_0046_moc(:, 2);
+y47 = HR_090715_0047_moc(:, 2);
+y48 = HR_090715_0048_moc(:, 2);
+y49 = HR_090715_0049_moc(:, 2);
+
+y50 = HR_090715_0050_moc(:, 2);
+y51 = HR_090715_0051_moc(:, 2);
+y52 = HR_090715_0052_moc(:, 2);
+y53 = HR_090715_0053_moc(:, 2);
+y54 = HR_090715_0054_moc(:, 2);
+y55 = HR_090715_0055_moc(:, 2);
+y56 = HR_090715_0056_moc(:, 2);
+y57 = HR_090715_0057_moc(:, 2);
+y58 = HR_090715_0058_moc(:, 2);
+y59 = HR_090715_0059_moc(:, 2);
+
+y60 = HR_090715_0060_moc(:, 2);
+y61 = HR_090715_0061_moc(:, 2);
+y62 = HR_090715_0062_moc(:, 2);
+y63 = HR_090715_0063_moc(:, 2);
+y64 = HR_090715_0064_moc(:, 2);
+y65 = HR_090715_0065_moc(:, 2);
+
+
+%%
+sum0 = y0 + y1 + y2 + y3 + y4 + y5 + y6 + y7 + y8 + y9;
+sum1 = y10 + y11 + y12 + y13 + y14 + y15 + y16 + y17 + y18 + y19;
+sum2 = y20 + y21 + y22 + y23 + y24 + y25 + y26 + y27 + y28 + y29;
+sum3 = y30 + y31 + y32 + y33 + y34 + y35 + y36 + y37 + y38 + y39;
+sum4 = y40 + y41 + y42 + y43 + y44 + y45 + y46 + y47 + y48 + y49;
+sum5 = y50 + y51 + y52 + y53 + y54 + y55 + y56 + y57 + y58 + y59;
+sum6 = y60 + y61 + y62 + y63 + y64 + y65;
+mean = (sum0 + sum1 + sum2 + sum3 + sum4 + sum5 + sum6) / 66;
+
+A0 = [y0'; y1'; y2'; y3'; y4'; y5'; y6'; y7'; y8'; y9'];
+A1 = [y10'; y11'; y12'; y13'; y14'; y15'; y16'; y17'; y18'; y19'];
+A2 = [y20'; y21'; y22'; y23'; y24'; y25'; y26'; y27'; y28'; y29'];
+A3 = [y30'; y31'; y32'; y33'; y34'; y35'; y36'; y37'; y38'; y39'];
+% A3 = [y30'; y31'; y32'; y33'; y34'; y36'; y37'; y38'; y39'];
+A4 = [y40'; y41'; y42'; y43'; y44'; y45'; y46'; y47'; y48'; y49'];
+A5 = [y50'; y51'; y52'; y53'; y54'; y55'; y56'; y57'; y58'; y59'];
+A6 = [y60'; y61'; y62'; y63'; y64'; y65'];
+% A6 = [y63'; y64'; y65'];
+A = [A0; A1; A2; A3; A4; A5; A6];
+
+%%
+% A = [A; A; A; A; A; A; A; A; A; A; A; A; A; A];
+covarianceMatrix = cov(A);
+
+% [D, p] = chol(covarianceMatrix);
+
+d = eig(covarianceMatrix);
+[eigenVector, eigenValue] = eig(covarianceMatrix);
+% sort eigenValue(with eigenVector)
+n = length(d);
+i = 1; j = 1;
+for j = 1 : n
+    for i = 2 : n
+        if d(i-1) < d(i)
+            temp = d(i);
+            d(i) = d(i-1);
+            d(i-1) = temp;
+            for k = 1 : 61          % exchange eigenVector according eigenValue
+                t = eigenVector(k, i);
+                eigenVector(k, i) = eigenVector(k, i-1);
+                eigenVector(k, i-1) = t;
+            end
+        end
+    end
+    n = n-1;
+end
+
+% normalize eigenVector
+[rowNum colNum] = size(A);
+for i = 1 : colNum
+    eigenVector(:, i) = eigenVector(:, i) / norm(A(:, i));
+end
+
+% d = sort(d, 'descend');
+
+% for i = 1 : 548
+%     if d(i) < 0
+%         d(i) = 0;
+%     end
+% end
+% d = (d - min(d))/(max(d)-min(d));
+
+
+% for i = 0 : 100
+%     yi = normrnd(0, d');
+%     resulti = inv(eigenVector) * yi' + mean;
+%     plot(x, resulti, '-');
+% end
+
+% Y1= normrnd(0,d');
+% result1 = inv(eigenVector) * Y1' + mean;
+hold all;
+rouSum = 0;
+N = 100;    % the sum of line
+for i = 1 : N
+    yi = normrnd(0, d');
+    resulti = inv(eigenVector') * yi' + mean;
+    plot(x, resulti, '-');
+    % compute the z-score
+    rouAvgI(1, i) = sum(resulti) / 61;
+    rouSum = rouSum + rouAvgI(1, i);
+end
+rouAvg = rouSum / N;
+deltaRou = std(rouAvgI);
+zi = (rouAvgI - rouAvg) / deltaRou;
+
+
+%%
+plot(x, mean, '*');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
