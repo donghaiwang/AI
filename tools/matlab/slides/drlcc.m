de150a0f2bc3a41ce46435b30ee8fd671072fe28
donghaiwang <1402499358@qq.com>
2019-03-21 21:14:33 +0800

    add latex2image.m

--- tools/matlab/slides/drlcc.m
+++ tools/matlab/slides/drlcc.m
@@ -12,6 +12,7 @@ function drlcc()
 % objects in this function, e.g., you can refer to 
 % mlreportgen.ppt.Presentation simply as Presentation.
 import mlreportgen.ppt.*;
+import mlreportgen.report.*
 
 % Create a cell array to hold images generate for this presentation. 
 % That will facilitate deleting the images at the end of presentation
@@ -88,12 +89,73 @@ replace(slide, 'Right Text', 'Network Architecture');
 adnetArchitectureImg = fullfile(imgPath, '2_2_adnetNet.png');
 replace(slide, 'Right Content', Picture(adnetArchitectureImg));
 
-%% 3.1 Add the last slide to the presentation based on Title and Content layout.
+%% 3.1 CDRL
 slide = add(pre, 'Title and Content');
 replace(slide, 'Title', 'Multi-Object Tracking Using Collaborative Deep Reinforcement Learning');
 
 cdrlImg = fullfile(imgPath, '3_1_CDRL.PNG');
 replace(slide, 'Content', Picture(cdrlImg));
+
+%
+slide = add(pre, 'Title and Content');
+replace(slide, 'Title', 'Collaborative Deep Reinforcement Learning');
+
+latexstr = 'd(p_i,p_j)=\alpha(1-g(p_i,p_j))+(1-\frac{f_i^Tf_j}{\left \| f_i \right \|_2 \left \| f_j \right \|_2})';
+distance2NeighborhoodImg = latex2image(latexstr);
+distance2DetectionLatex = 'd(p_i,p_j^*)=\alpha(1-g(p_i,p_j^*))+(1-\frac{f_i^Tf_j^*}{\left \| f_i \right \|_2 \left \| f_j^2 \right \|_2})';
+distance2DetectionImg = latex2image(distance2DetectionLatex);
+distance2NeighborhoodName = 'formula1.png';
+distance2DetectionName = 'formula2.png';
+replace(slide, 'Content', Picture(savedImgName));
+
+%
+slide = add(pre, 'Title and Table');
+replace(slide, 'Title', 'Collaborative Deep Reinforcement Learning');
+
+table1 = Table(3);
+
+tr1 = TableRow();
+tr1.Style = {Bold(true)};
+
+te1tr1 = TableEntry();
+p = Paragraph('first entry');
+% p.FontColor = 'red';
+append(te1tr1,p);
+
+te2tr1 = TableEntry();
+append(te2tr1,'second entry');
+
+te3tr1 = TableEntry();
+% te3tr1.Style = {FontColor('green')};
+append(te3tr1,'third entry');
+
+append(tr1,te1tr1);
+append(tr1,te2tr1);
+append(tr1,te3tr1);
+
+tr2 = TableRow();
+te1tr2 = TableEntry();
+% te1tr2.Style = {FontColor('red')};
+p = Paragraph('first entry');
+append(te1tr2,p);
+
+te2tr2 = TableEntry();
+append(te2tr2,'second entry');
+
+te3tr2 = TableEntry();
+% te3tr2.Style = {FontColor('green')};
+append(te3tr2,'third entry');
+
+append(tr2,te1tr2);
+append(tr2,te2tr2);
+append(tr2,te3tr2);
+
+append(table1,tr1);
+append(table1,tr2);
+
+contents = find(slide,'Table');
+replace(contents(1),table1);
+
 
 %% 4.1 RiID
 slide = add(pre, 'Title and Content');
@@ -109,6 +171,11 @@ replace(slide, 'Title', 'Multi-Target, Multi-Camera Tracking using Hierarchy Dee
 cdrlImg = fullfile(imgPath, '5_drlcc.png');
 replace(slide, 'Content', Picture(cdrlImg));
 
+
+%% End
+slide = add(pre, 'Title Slide');
+replace(slide, 'Title', 'Thank you');
+replace(slide, 'Subtitle', 'Email: haidong@hnu.edu.cn');
 
 
 %% Finally, close the presentation and open it in Windows
