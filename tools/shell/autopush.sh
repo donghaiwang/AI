c7c78b7d140ccfc1a92c2ec89daba89cde98bb03
donghaiwang <1402499358@qq.com>
2019-05-28 15:58:58 +0800

    auto commit

---
+++ tools/shell/autopush.sh
@@ -0,0 +1,9 @@
+# auto add, commit, push to default remote
+
+# add path to user path
+# vi ~/.bashrc
+# export PATH=$PATH:/home/d/workspace/AI/tools/shell
+# source ~/.bashrc
+git add .
+git commit -m 'auto commit'
+git push
