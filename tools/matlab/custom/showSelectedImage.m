302376350501389cf92c62b5c175f32122aebaa9
donghaiwang <1402499358@qq.com>
2019-06-03 17:03:01 +0800

    auto commit

---
+++ tools/matlab/custom/showSelectedImage.m
@@ -0,0 +1,22 @@
+%% add comment for a script by press "Alt + 2"
+% Home -> Favorites -> New Favorite -> 
+% Label: size
+% Code:  getSelectionSize
+% select 'Add to quick access toolbar' and 'Show label on quick acces
+% toolbar'.
+
+%% 
+% test code:
+% container = zeros(2, 3, 'single', 'gpuArray');
+% disp(container);
+
+% ws: 'base' or caller
+evalin('caller', 'addpath(pwd)');  % TODO: solve 'underfine function' defined in m script
+
+activeEditor = matlab.desktop.editor.getActive;
+
+selectedText = activeEditor.SelectedText;
+
+clc;
+eval(['imshow(', selectedText, ')']);
+
