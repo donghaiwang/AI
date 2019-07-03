88f834666b53e3e468b7cefdb614e3e5bcc74931
donghaiwang <1402499358@qq.com>
2019-07-03 16:30:52 +0800

    auto commit

--- study/matlab/parallelComp/parfevalDemo.m
+++ study/matlab/parallelComp/parfevalDemo.m
@@ -1,3 +1,16 @@
 p = gcp();
 f = parfeval(p, @magic, 1, 10);
 value = fetchOutputs(f);
+
+%%
+p = gcp();
+for idx = 1 : 10
+    f(idx) = parfeval(p, @magic, 1, idx);
+end
+
+magicResults = cell(1, 10);
+for idx = 1 : 10
+    [completedIdx, value] = fetchNext(f);
+    magicResults{completedIdx} = value;
+    fprintf('Got result with index: %d.\n', completedIdx);
+end
