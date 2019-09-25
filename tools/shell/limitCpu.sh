537b3d90453b0881213e3b71424e5e83318a091c
donghaiwang <1402499358@qq.com>
2019-09-25 16:37:02 +0800

    auto commit

--- tools/shell/limitCpu.sh
+++ tools/shell/limitCpu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # limit CPU usage when beyond limit  
 # Run example on background:
-# nohup ./limitCpu.sh &>/dev/nulr
+# nohup ./limitCpu.sh &>/dev/null
 
-ps aux | awk '{if($3>=200.0 && $1!="d") print $2}' | while read procid
+ps aux | awk '{if($3>=400.0 && $1!="d") print $2}' | while read procid
 do
     echo "jjj"|sudo -S cputool --cpu-limit=365 -p $procid
 done
