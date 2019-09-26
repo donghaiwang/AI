18a7dc7a6f2a7e2eefe3d6cdbb4a654936c70f19
donghaiwang <1402499358@qq.com>
2019-09-26 14:47:07 +0800

    auto commit

--- tools/shell/limitCpu.sh
+++ tools/shell/limitCpu.sh
@@ -3,10 +3,16 @@
 # Run example on background:
 # nohup ./limitCpu.sh &>/dev/null
 
-ps aux | awk '{if($3>=400.0 && $1!="d") print $2}' | while read procid
-do
-    echo "jjj"|sudo -S cputool --cpu-limit=365 -p $procid
+while true; do
+    ps aux | awk '{if($3>=210.0 && $1!="d") print $2}' | while read procid
+    do
+        echo "jjj"|sudo -S cputool --cpu-limit=200 -p $procid
+    done
+    sleep 5s
 done
+
+exit 0
+
 
 
 
