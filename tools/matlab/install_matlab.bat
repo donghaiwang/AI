e3b99b17210613282d8a122286209be64ab131ff
donghaiwang <1402499358@qq.com>
2019-11-30 19:44:29 +0800

    使用7zip解压安装包到当前文件夹，并安装到C:\matlab_2018a

--- tools/matlab/install_matlab.bat
+++ tools/matlab/install_matlab.bat
@@ -1,4 +1,7 @@
-start /wait C:\matlab_2018a_install\setup.exe -destinationFolder C:\matlab_2018a -fileInstallationKey 09806-07443-53955-64350-21751-41297 -agreeToLicense yes -outputFile C:\matlab_2018a_install\matlab_install.log -mode silent -activationPropertiesFile C:\matlab_2018a_install\license_standalone.lic -setFileAssoc false -desktopShortcut false -startMenuShortcut false /S
-xcopy C:\matlab_2018a_install\netapi32.dll C:\matlab_2018a\bin\win64
-xcopy C:\matlab_2018a_install\license_standalone.lic C:\matlab_2018a\licenses
+:: 在当前目录解压并安装matlab，安装的目录为C:\matlab_2018a
+:: TODO:  执行xcopy之前需要等待matlab安装程序结束
+7z x matlab_2018a_install.rar
+start /wait matlab_2018a_install\setup.exe -destinationFolder C:\matlab_2018a -fileInstallationKey 09806-07443-53955-64350-21751-41297 -agreeToLicense yes -outputFile matlab_2018a_install\matlab_install.log -mode silent -activationPropertiesFile matlab_2018a_install\license_standalone.lic -setFileAssoc false -desktopShortcut false -startMenuShortcut false /S
+xcopy matlab_2018a_install\netapi32.dll C:\matlab_2018a\bin\win64
+xcopy matlab_2018a_install\license_standalone.lic C:\matlab_2018a\licenses
 echo matlab 2018a install finished!
