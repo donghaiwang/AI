4fa5b3ef112f8a9ec66e4115bc1681cff06dc4b4
donghaiwang <1402499358@qq.com>
2020-02-12 18:26:36 +0800

    system boot

--- tools/matlab/custom/startup.m
+++ tools/matlab/custom/startup.m
@@ -1,6 +1,19 @@
 % run this command when matlab startup
-% Require add this path to PATH
-% dbstop if error
+
+%% Environment variables
+aiPath = fullfile('D:', 'workspace', 'AI');
+workPath = fullfile(aiPath, 'study', 'matlab', 'matlab');
+eval(['cd ', workPath]);
+clear aiPath workPath
+
+
+%% Require add this path to PATH
+% customPath = 'D:\workspace\AI\tools\matlab\custom';
+% addpath(customPath, '-end');
+% savepath
+
+%%
+dbstop if error
 if ispc
     tmp = 'D:/tmp';
 else
@@ -9,11 +22,19 @@ end
 
 currentPath = fileparts(mfilename('fullpath'));
 addpath(currentPath);
-% addpath(fullfile(currentPath, 'customCommand'));
 
-%% 
-% addpath('D:\software\matlab_third\dl');
-% cd D:\workspace\tracking\DeepCC
+
+%% System Boot
+status = system('C:\Program Files (x86)\EndNote X8\EndNote.exe &');
+system('C:\Program Files\JetBrains\PyCharm Community Edition 2019.3.1\bin\pycharm64.exe &');
+system('D:\software\MobaXterm Personal Edition\MobaXterm.exe &');
+
+system('C:\Program Files (x86)\Google\Chrome\Application\chrome.exe &');
+system('C:\Program Files (x86)\Adobe\Acrobat DC\Acrobat\Acrobat.exe &');
+system('C:\Program Files\Microsoft Office\Office16\WINWORD.EXE& ');
+
+% Select free GPU device if have GPU
+
 
 %% temporary operation
 % cd D:\workspace\data\MOT\devkit\utils
